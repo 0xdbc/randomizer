14b6af0fda0c01c85355ad3e0f7887b7a7cf3068
Harish <Echarish999@gmail.com>
2020-08-15 00:49:26 +0530

    Revert "Code refactoring"
    This reverts commit 9f11dc2b549ba3a00b02e5857b19f8f8d0ab7687.

--- src/main/java/com/mc/randomizer/services/RandomStringServiceImpl.java
+++ src/main/java/com/mc/randomizer/services/RandomStringServiceImpl.java
@@ -3,26 +3,13 @@ package com.mc.randomizer.services;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
 import java.util.Random;
 
-//To register in the context as a Spring bean & can be found during classpath scanning
-@Component
 public class RandomStringServiceImpl implements RandomStringService {
-
-    //HashSet<String> genartedStrings=new HashSet();
-    //(If we want to remove the duplicate generted number then we can enable the Set implemenation code )
-    /* Instead of writing the complete custom code we can use APache libraray function as well
-              RandomStringUtils.randomAlphanumeric(Size);
-     */
 
     @Override
     public String getRandomString(Integer size) {
-        /* Added small case as well . If we want to avoid collision in generated code then this will help .
-         But at the same time to remove collision , we need to use Set where every generated string we
-          will store as Key and we can keep on removing the first genarted code whne set is full (Similiar to what we do in Cache)
-         */
-        String ALPHANUM123 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJMNOPQRSTUVWXYZ123456790";
+        String ALPHANUM123 = "ABCDEFGHIJMNOPQRSTUVWXYZ123456790";
         StringBuilder sb = new StringBuilder();
         Random r = new Random();
         while (sb.length() < size) {
@@ -30,13 +17,6 @@ public class RandomStringServiceImpl implements RandomStringService {
             sb.append(ALPHANUM123.charAt(index));
         }
         String s = sb.toString();
-        // To avoide duplicate /Collision we can enable this code
-
-        /*if(!genartedStrings.contains(s))
-            genartedStrings.add(s);
-        else
-            getRandomString(size);
-            */
         return s;
     }
 }
